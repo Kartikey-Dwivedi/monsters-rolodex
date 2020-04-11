c5b72d958755073f422a0381444a1c54fed70bb9
Kartikey-Dwivedi <63398278+Kartikey-Dwivedi@users.noreply.github.com>
2020-04-11 17:31:19 +0530

    Updates

---
+++ precache-manifest.924ca8e061ee8eb5197248b84aa6fccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2615ef44406ea2653d0cf812a237fa09",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "91bdae1a69a3f4b4000c",
+    "url": "/monsters-rolodex/static/css/main.2cbc3b3d.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91bdae1a69a3f4b4000c",
+    "url": "/monsters-rolodex/static/js/main.7ffc7a3a.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
